dcb8550983620fb181a44b0965088f5b8684d084
SpcFORK <126259962+SpcFORK@users.noreply.github.com>
2024-01-10 21:57:24 +0000

    Fix

--- app/entry.js
+++ app/entry.js
@@ -1,14 +1,7 @@
 window.Main = class Main {
 
-  /*
-    This class automatically inherits:
-      - The entry element,
-      - The page pathname,
-      - The file location.
-
-    this.entry, this.path, this.entryPath,
-  */
-
+  static PageTitle = 'Main';
+  
   static async preload() {
 
   }
